596b38b2568cffef7ad3d497c77fde7878c261ae
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2026-07-02 12:14:01 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,5 +1,4 @@
 // BANCO DE DADOS ACESSO COMPARTILHADO
-// Você pode colar o link completo do YouTube. O sistema extrai o ID automaticamente!
 const musicDatabase = [
     { 
         id: 1, 
@@ -7,7 +6,8 @@ const musicDatabase = [
         composer: "Vinicius de Moraes / Toquinho", 
         book: "Arca de Noé", 
         pdfUrl: "partituras/a_casa.pdf", 
-        youtubeUrl: "https://www.youtube.com/watch?v=K3DEgVGA0Gs&list=RDK3DEgVGA0Gs&start_radio=1" 
+        youtubeUrl: "https://www.youtube.com/watch?v=K3DEgVGA0Gs&list=RDK3DEgVGA0Gs&start_radio=1",
+        mp3Url: "https://archive.org/details/the-matador-bastien-piano-basics-3"  // LINK DIRETO
     },
     { 
         id: 2, 
@@ -15,14 +15,17 @@ const musicDatabase = [
         composer: "Toquinho", 
         book: "Música na Infância Vol. 1", 
         pdfUrl: "partituras/o_caderno.pdf", 
-        youtubeUrl: "https://www.youtube.com/watch?v=_0mpldsbZdc" 
+        youtubeUrl: "https://www.youtube.com/watch?v=_0mpldsbZdc",
+        mp3Url: "https://archive.org/download/the-matador-bastien-piano-basics-3/The%20Matador_Bastien%20Piano%20Basics%203.mp3"  // LINK DIRETO
     }
 ];
-
-// Função auxiliar para extrair o ID do YouTube de qualquer formato de link
+d
+// Função auxiliar para extrair o ID do YouTube
 function getYouTubeId(url) {
     if (!url) return null;
     const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
     const match = url.match(regExp);
     return (match && match[2].length === 11) ? match[2] : null;
 }
+
+

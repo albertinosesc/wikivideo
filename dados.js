218dde039ee077d8ab49933fd0aba95aa81dc170
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2026-07-02 12:35:21 -0300

    Update mp3 URLs in music database

--- dados.js
+++ dados.js
@@ -1,13 +1,13 @@
 // BANCO DE DADOS ACESSO COMPARTILHADO
 const musicDatabase = [
-    { 
+   { 
         id: 1, 
         title: "A Casa", 
         composer: "Vinicius de Moraes / Toquinho", 
         book: "Arca de Noé", 
         pdfUrl: "partituras/a_casa.pdf", 
         youtubeUrl: "https://www.youtube.com/watch?v=K3DEgVGA0Gs&list=RDK3DEgVGA0Gs&start_radio=1",
-        mp3Url: "https://archive.org/details/the-matador-bastien-piano-basics-3"  // LINK DIRETO
+        mp3Url: "https://archive.org/download/the-matador-bastien-piano-basics-3/The%20Matador_Bastien%20Piano%20Basics%203.mp3"
     },
     { 
         id: 2, 
@@ -16,10 +16,11 @@ const musicDatabase = [
         book: "Música na Infância Vol. 1", 
         pdfUrl: "partituras/o_caderno.pdf", 
         youtubeUrl: "https://www.youtube.com/watch?v=_0mpldsbZdc",
-        mp3Url: "https://archive.org/download/the-matador-bastien-piano-basics-3/The%20Matador_Bastien%20Piano%20Basics%203.mp3"  // LINK DIRETO
+        mp3Url: "https://blog-imgs-201.fc2.com/n/b/m/nbmescolademusica/20260703001739f89.mp3"
     }
 ];
-d
+// <-- NÃO COLOQUE NADA AQUI!
+
 // Função auxiliar para extrair o ID do YouTube
 function getYouTubeId(url) {
     if (!url) return null;
@@ -27,5 +28,3 @@ function getYouTubeId(url) {
     const match = url.match(regExp);
     return (match && match[2].length === 11) ? match[2] : null;
 }
-
-
